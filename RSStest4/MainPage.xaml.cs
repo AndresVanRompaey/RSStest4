6716b915c2976149cd2cdbe8aff7f449748b6349
Jamie Twells <jtwells3@googlemail.com>
2018-05-31 12:58:09 +0100

    Adding missing files

--- RSStest4/MainPage.xaml.cs
+++ RSStest4/MainPage.xaml.cs
@@ -10,26 +10,32 @@ using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace RSStest4 {
+namespace RSStest4
+{
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page {
-        public MainPage() {
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e) {
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
             // Load the RSS file from the RSS URL
             XmlDocument.LoadFromUriAsync(new Uri("http://www.verkeerscentrum.be/rss/1-LOS.xml"))
                 .Completed = XmlLoadedAsync;
         }
 
-        private async Task UpdateTextBox(string data) {
+        private async Task UpdateTextBox(string data)
+        {
             await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => txtRSS.Text = data);
         }
 
-        private async void XmlLoadedAsync(IAsyncOperation<XmlDocument> asyncInfo, AsyncStatus asyncStatus) {
+        private async void XmlLoadedAsync(IAsyncOperation<XmlDocument> asyncInfo, AsyncStatus asyncStatus)
+        {
             var rssXDoc = asyncInfo.GetResults();
 
             // Parse the Items in the RSS file
@@ -38,17 +44,12 @@ namespace RSStest4 {
             var rssContent = new StringBuilder();
 
             // Iterate through the items in the RSS file
-            foreach (var rssNode in rssNodes) {
+            foreach (var rssNode in rssNodes)
+            {
                 var rssSubNode = rssNode.SelectSingleNode("title");
                 var title = rssSubNode != null ? rssSubNode.InnerText : "";
 
-                rssSubNode = rssNode.SelectSingleNode("link");
-                var link = rssSubNode != null ? rssSubNode.InnerText : "";
-
-                rssSubNode = rssNode.SelectSingleNode("description");
-                var description = rssSubNode != null ? rssSubNode.InnerText : "";
-
-                rssContent.AppendLine($"<a href='{link}'>\n\t{title}\n</a><br>\n{description}");
+                rssContent.AppendLine($"{title}");
             }
 
             // Return the string that contain the RSS items
