7bb0d564a0db8c2adef8ed1aea5e35579ac917fe
Jamie Twells <jtwells3@googlemail.com>
2018-05-31 13:37:35 +0100

    parsing only the first title in the list

--- RSStest4/MainPage.xaml.cs
+++ RSStest4/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,56 +12,36 @@ using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace RSStest4
-{
+namespace RSStest4 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        private void Button_Click(object sender, RoutedEventArgs e) {
             // Load the RSS file from the RSS URL
             XmlDocument.LoadFromUriAsync(new Uri("http://www.verkeerscentrum.be/rss/1-LOS.xml"))
                 .Completed = XmlLoadedAsync;
         }
 
-        private async Task UpdateTextBox(string data)
-        {
+        private async Task UpdateTextBox(string data) {
             await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => txtRSS.Text = data);
         }
 
-        private async void XmlLoadedAsync(IAsyncOperation<XmlDocument> asyncInfo, AsyncStatus asyncStatus)
-        {
+        private async void XmlLoadedAsync(IAsyncOperation<XmlDocument> asyncInfo, AsyncStatus asyncStatus) {
             var rssXDoc = asyncInfo.GetResults();
 
             // Parse the Items in the RSS file
             var rssNodes = rssXDoc.SelectNodes("rss/channel/item");
 
-            var rssContent = new StringBuilder();
-
-            // Iterate through the items in the RSS file
-            foreach (var rssNode in rssNodes)
-            {
-                var rssSubNode = rssNode.SelectSingleNode("title");
-                var title = rssSubNode != null ? rssSubNode.InnerText : "";
-
-                rssContent.AppendLine(GetTrendFromTitle(title));
-            }
+            var rssSubNode = rssNodes.First().SelectSingleNode("title");
+            var title = rssSubNode != null ? rssSubNode.InnerText : "";
 
             // Return the string that contain the RSS items
-            await UpdateTextBox(rssContent.ToString());
-        }
-
-        private string GetTrendFromTitle(string title) {
-            var match = Regex.Match(title, @"Trend: (stijgend|stabiel|dalend)");
-            var trend = match.Groups[1];
-            return trend.Value;
+            await UpdateTextBox(title);
         }
     }
 }
