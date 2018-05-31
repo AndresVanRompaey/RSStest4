0038bbc78318544f029b12b385dcc891609c877e
Jamie Twells <jtwells3@googlemail.com>
2018-05-31 12:23:48 +0100

    Fixed the load data

--- RSStest4/MainPage.xaml.cs
+++ RSStest4/MainPage.xaml.cs
@@ -1,51 +1,58 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.Data.Xml.Dom;
+using Windows.Foundation;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace RSStest4
-{
+namespace RSStest4 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Windows.Data.Xml.Dom.XmlDocument rssXDoc = new Windows.Data.Xml.Dom.XmlDocument();
-
+        private void Button_Click(object sender, RoutedEventArgs e) {
             // Load the RSS file from the RSS URL
-            rssXDoc.LoadXml("http://www.verkeerscentrum.be/rss/1-LOS.xml");
+            XmlDocument.LoadFromUriAsync(new Uri("http://www.verkeerscentrum.be/rss/1-LOS.xml"))
+                .Completed = XmlLoadedAsync;
+        }
+
+        private async Task UpdateTextBox(string data) {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => txtRSS.Text = data);
+        }
+
+        private async void XmlLoadedAsync(IAsyncOperation<XmlDocument> asyncInfo, AsyncStatus asyncStatus) {
+            var rssXDoc = asyncInfo.GetResults();
 
             // Parse the Items in the RSS file
-            Windows.Data.Xml.Dom.XmlNodeList rssNodes = rssXDoc.SelectNodes("rss/channel/item");
+            var rssNodes = rssXDoc.SelectNodes("rss/channel/item");
 
-            StringBuilder rssContent = new StringBuilder();
+            var rssContent = new StringBuilder();
 
             // Iterate through the items in the RSS file
-            foreach (IXmlNode rssNode in rssNodes)
-            {
-                IXmlNode rssSubNode = rssNode.SelectSingleNode("title");
-                string title = rssSubNode != null ? rssSubNode.InnerText : "";
+            foreach (var rssNode in rssNodes) {
+                var rssSubNode = rssNode.SelectSingleNode("title");
+                var title = rssSubNode != null ? rssSubNode.InnerText : "";
 
                 rssSubNode = rssNode.SelectSingleNode("link");
-                string link = rssSubNode != null ? rssSubNode.InnerText : "";
+                var link = rssSubNode != null ? rssSubNode.InnerText : "";
 
                 rssSubNode = rssNode.SelectSingleNode("description");
-                string description = rssSubNode != null ? rssSubNode.InnerText : "";
+                var description = rssSubNode != null ? rssSubNode.InnerText : "";
 
-                rssContent.Append("<a href='" + link + "'>" + title + "</a><br>" + description);
+                rssContent.AppendLine($"<a href='{link}'>\n\t{title}\n</a><br>\n{description}");
             }
 
             // Return the string that contain the RSS items
-            txtRSS.Text = rssContent.ToString();
+            await UpdateTextBox(rssContent.ToString());
         }
     }
 }
